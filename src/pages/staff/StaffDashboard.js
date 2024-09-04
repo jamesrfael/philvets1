ca31c6e83e47f3316aa3a8e9d171231336a698ca
MryPrcs03 <navaresmaryprecious@gmail.com>
2024-09-04 19:14:49 +0800

    StaffDashboard modified

--- src/pages/staff/StaffDashboard.js
+++ src/pages/staff/StaffDashboard.js
@@ -1,68 +1,54 @@
+// src/pages/AdminDashboard.js
+
 import React from "react";
 import styled from "styled-components";
-import StaffLayoutHS from "../../components/Layout/StaffLayoutHS";
-import { TbUserDollar, TbCategory, TbPackage } from "react-icons/tb";
-import { LuWarehouse } from "react-icons/lu";
-import { GrGroup } from "react-icons/gr";
+import { Link } from "react-router-dom";
+
+import StaffLayoutHS from "../../components/Layout/StaffLayoutHS"
 import HighestSellingProducts from "../../components/AdminDashboard/HighestSellingProducts";
 import LatestSales from "../../components/AdminDashboard/LatestSales";
 import RecentlyAddedProducts from "../../components/AdminDashboard/RecentlyAddedProducts";
-
-const cardData = [
-  {
-    title: "Products",
-    number: "120",
-    icon: <TbPackage />,
-    color: "#ff6961",
-  },
-  {
-    title: "Customers",
-    number: "1,234",
-    icon: <TbUserDollar />,
-    color: "#ffb347",
-  },
-  {
-    title: "Suppliers",
-    number: "23",
-    icon: <LuWarehouse />,
-    color: "#6da6ba",
-  },
-  {
-    title: "Staffs",
-    number: "12",
-    icon: <GrGroup />,
-    color: "#c893c8",
-  },
-  {
-    title: "Category",
-    number: "5",
-    icon: <TbCategory />,
-    color: "#6b5b95",
-  },
-];
+import LowestStocks from "../../components/AdminDashboard/LowestStocks";
+import CardLowStocks from "../../components/CardsData/CardLowStocks";
+import CardTotalProducts from "../../components/CardsData/CardTotalProducts";
+import CardTotalSales from "../../components/CardsData/CardTotalSales";
 
 const StaffDashboard = () => {
   return (
     <StaffLayoutHS>
       <CardContainer>
-        {cardData.map((card, index) => (
-          <Card key={index}>
-            <CardContent>
-              <IconWrapper color={card.color}>{card.icon}</IconWrapper>
-              <CardTitle>{card.title}</CardTitle>
-              <CardNumber>{card.number}</CardNumber>
-            </CardContent>
-          </Card>
-        ))}
+        <CardTotalProducts />
+        <CardTotalSales />
+        <CardLowStocks />
       </CardContainer>
       <TablesContainer>
-        <HighestSellingProducts />
-        <LatestSales />
-        <RecentlyAddedProducts />
+        <Row>
+          <StyledLink to="/admin/products">
+            <HighestSellingProducts />
+          </StyledLink>
+          <StyledLink to="/admin/sales">
+            <LatestSales />
+          </StyledLink>
+        </Row>
+        <Row>
+          <StyledLink to="/admin/products">
+            <RecentlyAddedProducts />
+          </StyledLink>
+          <StyledLink to="/admin/inventory">
+            <LowestStocks />
+          </StyledLink>
+        </Row>
       </TablesContainer>
     </StaffLayoutHS>
   );
 };
+
+// Styled Link to ensure it does not affect table styling
+const StyledLink = styled(Link)`
+  text-decoration: none; /* Remove underline from link */
+  display: block; /* Make the link a block element to ensure it covers the entire table */
+  width: 100%; /* Ensure it takes up full width */
+`;
 
 const CardContainer = styled.div`
   display: flex;
@@ -71,84 +57,20 @@ const CardContainer = styled.div`
   margin: 0 auto;
   gap: 1rem;
   max-width: 1000px;
-
-  @media (max-width: 900px) {
-    flex-direction: column;
-    align-items: center;
-  }
-`;
-
-const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* Center content */
-  justify-content: center;
-  background-color: white;
-  border-radius: 0.5rem;
-  padding: 1rem;
-  margin: 0.5rem;
-  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
-  width: 220px;
-  height: 150px;
-
-  @media (max-width: 480px) {
-    width: 90%;
-    height: auto;
-    padding: 1rem;
-  }
-`;
-
-const CardContent = styled.div`
-  text-align: center; /* Center text */
-`;
-
-const IconWrapper = styled.div`
-  background-color: ${(props) => props.color};
-  color: white;
-  border-radius: 50%;
-  padding: 0.5rem;
-  margin-bottom: 0.5rem;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-
-  svg {
-    width: 24px;
-    height: auto;
-    color: white;
-  }
-
-  @media (max-width: 480px) {
-    svg {
-      width: 28px;
-    }
-  }
-`;
-
-const CardTitle = styled.h2`
-  font-size: 1rem;
-  font-weight: 400;
-
-  @media (max-width: 480px) {
-    font-size: 1.1rem;
-  }
-`;
-
-const CardNumber = styled.p`
-  font-size: 1.9rem;
-  font-weight: bold;
-
-  @media (max-width: 480px) {
-    font-size: 2rem;
-  }
 `;
 
 const TablesContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   margin: 2rem auto;
   max-width: 1000px;
   width: 100%;
+  gap: 1rem;
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
   gap: 1rem;
 
   @media (max-width: 1000px) {
