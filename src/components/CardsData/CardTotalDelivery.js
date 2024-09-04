0bcbf3f5a116c6fa3017182d3ceef756894cd359
jamesrfael <122792810+jamesrfael@users.noreply.github.com>
2024-09-05 07:59:49 +0800

    Update CardTotalDelivery.js

--- src/components/CardsData/CardTotalDelivery.js
+++ src/components/CardsData/CardTotalDelivery.js
@@ -1,22 +1,22 @@
 // src/components/CardsData/CardTotalDelivery.js
-
 import React from "react";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Card from "../Layout/Card"; // Import the reusable Card component
+import { deliveries } from "../../pages/data/DeliveryData"; // Import the delivery data
 import styled from "styled-components";
 import { FaTruck } from "react-icons/fa"; // Import an icon from react-icons
 
-const CardTotalDelivery = ({ totalDeliveries }) => {
-//   const navigate = useNavigate();
+const CardTotalDelivery = () => {
+  const navigate = useNavigate();
 
   // Calculate the total number of deliveries
-  const displayTotalDeliveries = totalDeliveries !== undefined ? totalDeliveries : 0;
+  const totalDeliveries = deliveries.length;
 
   return (
-    <CardContainer>
+    <CardContainer onClick={() => navigate('/admin/delivery')}>
       <Card
         label="Total Deliveries"
-        value={displayTotalDeliveries} // Display the total number of deliveries
+        value={totalDeliveries} // Display the total number of deliveries
         icon={<FaTruck />} // Add the icon here
       />
     </CardContainer>
